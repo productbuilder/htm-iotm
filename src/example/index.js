a52892b0a4b964a5a8f2b7581a717030bd641792
tangelder <joost@moobels.com>
2026-03-07 18:02:58 +0100

    update

--- src/example/index.js
+++ src/example/index.js
@@ -1,19 +1,29 @@
 import { customTranslations } from "./i18n.js";
 
-window.IOTM_PARTICIPATION_WIDGET_CONFIG = {
-	translations: {
-		buttons: {
-			readPrivacy: {
-				en: "Review privacy details",
-				nl: "Bekijk privacyinformatie",
-			},
-		},
-	},
-};
-
 await import("../app/index.js");
 
-const customWidget = document.querySelector("#custom-widget");
-if (customWidget) {
-	customWidget.translations = customTranslations;
+const textWidget = document.querySelector("#text-widget");
+if (textWidget) {
+	textWidget.translations = customTranslations;
+}
+
+const combinedWidget = document.querySelector("#combined-widget");
+if (combinedWidget) {
+	combinedWidget.translations = {
+		...customTranslations,
+		buttons: {
+			...customTranslations.buttons,
+			readPrivacy: {
+				en: "Check privacy terms",
+				nl: "Bekijk privacyvoorwaarden",
+			},
+		},
+		intro: {
+			...customTranslations.intro,
+			descriptionOne: {
+				en: "Help preserve city memory with your own photos, videos, and audio.",
+				nl: "Help het stadsgeheugen bewaren met je eigen foto's, video's en audio.",
+			},
+		},
+	};
 }
